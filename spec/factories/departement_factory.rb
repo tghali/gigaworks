c4212525618b2b24acae7cbe5444ed1064d85b7e
ilpoldo <ilpoldo@gmail.com>
2011-02-22 15:31:24 +0000

    account creation feature

--- spec/factories/departement_factory.rb
+++ spec/factories/departement_factory.rb
@@ -1,7 +1,6 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
-Factory.define :departement do |f|
-  f.departement_code 1
-  f.user_id 1
-  f.role "MyString"
+Factory.define :departement do |d|
+  d.name :sales
+  d.user { |d| d.association :user }
 end
