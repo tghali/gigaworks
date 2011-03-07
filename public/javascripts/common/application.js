95e281dc7bdd2001d0844704945ce382ddf8443e
ilpoldo <ilpoldo@gmail.com>
2011-03-07 21:01:51 +0000

    preparing for tuesday upload

--- public/javascripts/common/application.js
+++ public/javascripts/common/application.js
@@ -31,9 +31,9 @@ $(document).ready(function(){
   // });
   
   // Just to warm up my js:
-  (function(test_paragraph){
-    test_paragraph.html(test_paragraph.attr("data-message"));
-  })($('p[data-function=jstest]'));
+  // (function(test_paragraph){
+  //   test_paragraph.html(test_paragraph.attr("data-message"));
+  // })($('p[data-function=jstest]'));
   
   //Elastic Layout
   function fill(){
