532ccb5954c5bddffc2a341981073c264657e05f
sharma <sarma@.(none)>
2011-06-03 18:39:46 +0530

    commit for allowing docx format and delete translation changes

--- app/helpers/documents/documents_helper.rb
+++ app/helpers/documents/documents_helper.rb
@@ -1,18 +1,27 @@
 module Documents::DocumentsHelper
 
 def calculate_document_size(doc)
+  begin
 	filesize = doc.document_file_size
 	if Integer(filesize) >  1024
 	(Integer(filesize) / 1024).to_s + " Mb"
 	else
 		filesize + " kb"
 	end
+    rescue
+	  return "There is a problem to show file size."
+    end
 end
 
+
 def calculate_wordcount(doc)
+  begin
 	data = open(doc.document.url(:original)).read
 	word = (data.to_s).split(" ")
 	return word.length
+   rescue
+	   return "There is a problem to show word count."
+   end
 end
 
 end
