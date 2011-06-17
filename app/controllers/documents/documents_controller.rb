40a1b8049f231d6634fdb623dcc354a939257a36
sharma <sarma@.(none)>
2011-06-17 20:21:17 +0530

    commit for modifications in groups

--- app/controllers/documents/documents_controller.rb
+++ app/controllers/documents/documents_controller.rb
@@ -128,19 +128,24 @@ end
 ##### Google Doc API on 13-06-2011
 
 def googledoc_list
-	  @source_documents = SourceDocument.find(:all)
-	  #~ @documents = @account.files
+  
+  group = Group.find(1)
+  group.destroy
+  render :text => "success" and return
+  
+	  #~ @source_documents = SourceDocument.find(:all)
+	  #~ ## #~ @documents = @account.files
 	  
-	 if not params[:folder_id]
-		#Display all files and root folders
-		  @documents = @account.files
-		  @folders = @account.folders.select{|f| !f.parent } #display only root folders
-	  else
-		    #Display only files and folders contained by folder_id
-		    @folder = Folder.find(@account, {:id => params[:folder_id]})
-		    @documents = @folder.files
-		    @folders = @folder.folders
-	  end	  
+	 #~ if not params[:folder_id]
+		#~ #Display all files and root folders
+		  #~ @documents = @account.files
+		  #~ @folders = @account.folders.select{|f| !f.parent } #display only root folders
+	  #~ else
+		    #~ #Display only files and folders contained by folder_id
+		    #~ @folder = Folder.find(@account, {:id => params[:folder_id]})
+		    #~ @documents = @folder.files
+		    #~ @folders = @folder.folders
+	  #~ end	  
 end
   
 def view
