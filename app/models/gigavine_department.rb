a69062df7d41e96df389b002b7b8bb62642b24a8
sharma <sarma@.(none)>
2011-06-24 10:44:28 +0530

    commit for employe validations

--- app/models/gigavine_department.rb
+++ app/models/gigavine_department.rb
@@ -2,6 +2,6 @@ class GigavineDepartment < ActiveRecord::Base
 	belongs_to    :author,     :class_name => 'User'
 	
 	validates_presence_of :name, :description #:department_id,
-	#~ validates_uniqueness_of :department_id
+	validates_uniqueness_of :name
 	
 end
