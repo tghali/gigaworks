86071f604d2d8db21747b62acfa6cbdcdb645e9e
sharma <sarma@.(none)>
2011-06-24 09:54:34 +0530

    commit for modification in gigavine_department model

--- app/models/gigavine_department.rb
+++ app/models/gigavine_department.rb
@@ -1,7 +1,7 @@
 class GigavineDepartment < ActiveRecord::Base
 	belongs_to    :author,     :class_name => 'User'
 	
-	validates_presence_of :department_id, :name, :description
-	validates_uniqueness_of :department_id
+	validates_presence_of :name, :description #:department_id,
+	#~ validates_uniqueness_of :department_id
 	
 end
