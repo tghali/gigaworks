db374345ed769d29889d3cf9eff5f298b913905a
sharma <sarma@.(none)>
2011-06-30 18:58:27 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -152,6 +152,7 @@ Gigavine::Application.routes.draw do
     resources :clients
     resources :leads
     resources :employees
+    resources :talents
     resources :departments
     resources :languages
     resources :skills
