7693e02830d4b468429f9d93df4628d3e064258a
sharma <sarma@.(none)>
2011-08-12 15:50:12 +0530

    commit for add the design changes and add new term

--- app/helpers/schedule/schedule_helper.rb
+++ app/helpers/schedule/schedule_helper.rb
@@ -1,11 +1,16 @@
 module Schedule::ScheduleHelper
 
 def get_arabic_data(sentence_id)
-	tpair = TranslationPair.find(:first,:conditions => ["sentence_id = #{sentence_id} AND language_code = 1"])
-	if tpair
-	     return tpair.text
+	tpairs = TranslationPair.find(:all,:conditions => ["sentence_id = #{sentence_id} AND language_code = 1"])
+	if !tpairs.blank?
+		finalcode = ""
+			    for pair in tpairs	
+			      finalcode = tpairs.size > 1	? finalcode + pair.text + " / " : finalcode + pair.text 
+			  end
+		      return finalcode
+
 	else
-		return "None"
+		return " "
 	end
 end
 
