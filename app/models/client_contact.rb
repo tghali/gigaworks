f45003a231a91e0b739d846546c1201a6fb906fd
sharma <sarma@.(none)>
2011-08-19 15:19:54 +0530

    commit for modifications in user signup and invitation

--- app/models/client_contact.rb
+++ app/models/client_contact.rb
@@ -1,3 +1,16 @@
 class ClientContact < ActiveRecord::Base
-	  has_one    :client_contact_invite, :foreign_key => 'recipient_id', :dependent => :destroy
+	  has_one :client_contact_invite, :foreign_key => 'recipient_id', :dependent => :destroy
+	  
+	 validates :first_name,
+	    :presence => true,
+	    :uniqueness => {:if => :first_name?}
+	  
+	  validates :last_name,:organization,:role,
+	     :presence => true
+
+	  validates :email,   
+            :presence => true,   
+	    :uniqueness => { :scope => :gigaclient_id },	
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i , :message => "is invalid format", :if => :email?}
+
 end
