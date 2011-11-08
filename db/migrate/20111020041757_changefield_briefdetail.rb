618e7ee7f7416c89de476f2f7c7d9c400418d501
sharma <sarma@.(none)>
2011-11-08 17:43:22 +0530

    commit for migration changes in master

--- db/migrate/20111020041757_changefield_briefdetail.rb
+++ db/migrate/20111020041757_changefield_briefdetail.rb
@@ -1,11 +1,11 @@
 class ChangefieldBriefdetail < ActiveRecord::Migration
   def self.up
-	  change_table :brief_details do |t|
-		t.rename :string, :section_name
-	end
+	  #~ change_table :brief_details do |t|
+		#~ t.rename :string, :section_name
+	#~ end
   end
 
   def self.down
-	  t.rename  :section_name, :string
+	  #~ t.rename  :section_name, :string
   end
 end
