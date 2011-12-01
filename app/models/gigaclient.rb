5b2702e509571627e08d7972bf35fe0a77cef7b3
sharma <sarma@.(none)>
2011-12-01 19:51:01 +0530

    changes in network reg form validation and client reg username generation

--- app/models/gigaclient.rb
+++ app/models/gigaclient.rb
@@ -40,21 +40,64 @@ class Gigaclient < ActiveRecord::Base
  after_save :gigausers
 	
 	
-	def gigausers
+	#~ def gigausers
+    #~ @gfind = Gigauser.find_by_sql("SELECT id FROM gigausers order by id DESC limit 1")
+    #~ guser = Gigauser.new
+    #~ guser.id = @gfind[0]['id'] + 1
+    #~ guser.username = self.username
+    #~ guser.password = self.password
+    #~ guser.password_confirmation = self.password_confirmation    
+    #~ guser.first_name = self.first_name
+    #~ guser.last_name = self.last_name
+    #~ guser.gigaclient_id = self.id
+    #~ guser.role = "Client"
+    #~ guser.email = self.email
+    #~ guser.save
+		# Gigauser.create(:first_name =>self.first_name , :last_name =>self.last_name , :gigaclient_id => self.id, :role_id => 5,:email =>self.email )
+   #~ end
+   
+   
+   def gigausers
+    
     @gfind = Gigauser.find_by_sql("SELECT id FROM gigausers order by id DESC limit 1")
     guser = Gigauser.new
     guser.id = @gfind[0]['id'] + 1
+    puts " hellllllooooooooooooo  #{guser.id}"
+    # from talents registration
+    if self.username
     guser.username = self.username
+    #from network registration
+    else
+     puts "i got it...#{self.first_name[0,1]}"
+    guser.username=self.first_name[0,1] + self.last_name
+    if Gigauser.find_by_username(guser.username)
+       guser.username=self.first_name + self.last_name
+       if Gigauser.find_by_username(guser.username)
+          guser.username = self.first_name + "." + self.last_name
+       end
+    end
+  end
+      # from talents registration
+    if self.password
     guser.password = self.password
-    guser.password_confirmation = self.password_confirmation    
+       #from network registration
+    else
+     guser.password = '12345678'
+    end
+    if self.password_confirmation
+    guser.password_confirmation = self.password_confirmation
+    else
+     guser.password_confirmation = '12345678'
+    end
+    #guser.password_confirmation = self.password_confirmation
     guser.first_name = self.first_name
     guser.last_name = self.last_name
     guser.gigaclient_id = self.id
     guser.role = "Client"
     guser.email = self.email
-    guser.save
+    guser.save!
 		 #~ Gigauser.create(:first_name =>self.first_name , :last_name =>self.last_name , :gigaclient_id => self.id, :role_id => 5,:email =>self.email )
-	end
+end     
 	
 	
 	
